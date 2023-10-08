624e965ff507e71adaa3f6c17ae8a1c49c236932
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-08 19:15:22 +0200

    Solved and added 14 components

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -1,10 +1,11 @@
 import Hello from "./Hello";
+import Message from "./Message";
 
 const App = () => {
     return (
         <>
         <Hello />
-        <Hello />
+        <Message />
         </>
     )
 }
