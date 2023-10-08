cd23ca17ed16ba4588aeb583a7cba94665a26fe2
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-08 16:36:39 +0200

    Added and solver JSX3

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -1,3 +1,13 @@
 import { useState } from 'react'
 
+function App () {
+  const sayHello = (name) =>{
+    return <h1>Hello, {name}!</h1>
+  }
+
+    return  (<>
+              {sayHello()}
+            </>)
+}
+// Senza dare un parametro a Say hello verrà printato solo "Hello, ! senza il nome"
 export default App
