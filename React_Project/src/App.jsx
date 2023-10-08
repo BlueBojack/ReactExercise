22521ca6db0934d4a9685e15b6857fe9f224e18f
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-08 16:42:11 +0200

    Added and solved JSX4

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -1,3 +1,19 @@
 import { useState } from 'react'
 
+function App() {
+
+  const sayHello = (name) => {
+    return (`Hello, ${name}!`)
+  };
+  const jsxExpression = <h1>Curtis</h1>
+
+  return (
+    <>
+      {sayHello(jsxExpression)}
+    </>
+  )
+}
+
+// Riporterà [object, object] invece che il vero contenuto
+
 export default App
