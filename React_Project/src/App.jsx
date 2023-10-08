67eaa351c50f25a5c23fd3403cd924e430474deb
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-08 21:10:51 +0200

    Added and solve 24 Conditional rendering

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -5,7 +5,7 @@ import Welcome from "./Welcome";
 const App = () => {
     return (
         <>
-        <Welcome name= {<strong>Curtis</strong>} age={24}/>
+        <Welcome name= "John" age={24}/>
         </>
     )
 }
