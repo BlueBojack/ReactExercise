f7eae07686bd806b7825ea6c9eca51c5738351e4
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-08 16:54:17 +0200

    Added and solved JSX5

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -1,3 +1,14 @@
 import { useState } from 'react'
 
+function App () {
+
+  const sum = (a, b) =>{
+    return a + b;
+  }
+
+  return (
+    <h2>La somma è {sum(10,50)}</h2>
+  )
+}
+
 export default App
