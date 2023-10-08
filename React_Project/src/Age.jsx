ee8d18b86186006158c44a4507cb46b872a9808d
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-08 21:15:05 +0200

    Addedand solved 25 conditional rendering

--- React_Project/src/Age.jsx
+++ React_Project/src/Age.jsx
@@ -1,7 +1,10 @@
 const Age =({age}) => {
     return (
         <>
-            <p>Your age is {age}</p>
+        { age > 18 ?
+            (<p>Your age is {age}</p>) :
+            (<p>You are very young!</p>)
+        }
         </>
     )
 }
