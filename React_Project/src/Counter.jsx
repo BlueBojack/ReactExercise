3d35678ab9b39fb839d75a6ddc885b794d89b840
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-09 17:09:04 +0200

    Added and solved 30 state

--- React_Project/src/Counter.jsx
+++ React_Project/src/Counter.jsx
@@ -4,7 +4,8 @@ const Counter = () => {
     const [counter, setCounter] = useState(0);
 
     const handleAddOne = () => setCounter(counter + 1)
-        
+// Preferibilmente dovrebbe essere una funzione rispetto ad una immediate value perchè in questo modo si può usare il
+// codice in maniera più dinamica rispetto al value fisso da cambiare ogni volta.        
 
     return(
         <>
