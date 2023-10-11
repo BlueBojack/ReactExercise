cc8f5bfe2514479373e4ae0ec7f69a86835c3e97
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-11 17:42:48 +0200

    Added and solved 33

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -4,13 +4,9 @@ import { useState } from "react";
 
 const App = () => {
 
-    const [counter, setCounter] = useState(0);
-
-    const handleAddOne = () => setCounter(counter + 1)
-
     return(
             <>
-                <Counter counter={counter} setCounter={handleAddOne}/>
+                <Counter value ={5} reset={69}/>
              </>
     )   
 }
--- React_Project/src/Counter.jsx
+++ React_Project/src/Counter.jsx
@@ -1,11 +1,33 @@
 import CounterDisplay from "./CounterDisplay"
+import { useState } from "react";
 
-const Counter = ({counter, setCounter}) => {
+const Counter = (props) => {
+
+    const [count, setCount] = useState(props.reset)
+  
+    const handleIncrease = () =>{
+        setCount(
+            count + props.value
+        )
+    }
+
+    const handleDecrease = () =>{
+        setCount(
+            count - props.value
+        )
+    }
+
+    const handleReset = () =>{
+        setCount(props.reset)
+    }
 
     return(
-        <>
-            <CounterDisplay counter={counter} />
-            <button onClick={setCounter}>Add one to the counter!</button>
+        <> 
+            < CounterDisplay counter = {count} />
+            <button onClick={handleIncrease}>Increase the counter</button>
+            <button onClick={handleDecrease}>Decrease the counter</button>
+            <button onClick={handleReset}>Reset the counter</button>
+            
         </>
     )
 }
