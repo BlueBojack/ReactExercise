41e1e89de40f846294b917983cdfc458861631d9
BlueBojack <curtis.ducasse1g@gmail.com>
2023-11-08 19:48:28 +0100

    added 69

--- React_Project/src/Container.jsx
+++ React_Project/src/Container.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react"
 import "./Container.css"
 
 const Container = ({children, title}) => {
-    return (
-        <>
-            <div className="container">
-                <div>{title}</div>
-                {children}
-            </div>
-        </>
-    )
-}
+    const[hide, setHide] = useState(true)
+
+    const hideHandle = () => {
+        setHide((t) => !t);
+      };
+
+      return (
+        <div className="container">
+          <div className="container-btn">
+            <button onClick={hideHandle}>Toggle</button>
+          </div>
+          {hide && (
+            <>
+              <div className="container-title">{title}</div>
+              <div className="container-content">{children}</div>
+            </>
+          )}
+        </div>
+      );
+    }
 
 export default  Container
