d4fa647f60c636d455ec32707e7138f956adc165
BlueBojack <curtis.ducasse1g@gmail.com>
2023-11-09 16:09:33 +0100

    added 82

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
-import FilteredList from "./FilteredList";
+import  { Routes, Route} from "react-router-dom";
+import Welcome from "./Welcome"
 
 
 function App() {
 
-  const people = [
-    { id: 1, name: "Curtis", age: 24 },
-    { id: 2, name: "Giancarlo", age: 70 },
-    { id: 3, name: "Flavio", age: 43 },
-    { id: 4, name: "Giuseppe", age: 90 },
-    { id: 5, name: "Giada", age: 27 },
-    { id: 6, name: "Monica", age: 65 },
-  ];
-
   return (
     <>
-        <FilteredList  list={people}/>
+      <Routes>
+        <Route path="/" element={<Welcome name="Curtis" />} />
+      </Routes>
     </>
   );
 }
