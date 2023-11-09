afbeda6d9e0d16d4aae2f0f812bcf803fa7ef7b3
BlueBojack <curtis.ducasse1g@gmail.com>
2023-11-09 15:03:22 +0100

    added 78

--- React_Project/src/useGithubUser.jsx
+++ React_Project/src/useGithubUser.jsx
@@ -2,27 +2,29 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export function useGithubUser(username) {
-const [data, setData] = useState(null);
-const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-
-
-const fetchData = async () => {
-  try {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    if (!response.ok) {
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
         throw new Error("Errore nella richiesta");
       }
       const dataJSON = await response.json();
       setData(dataJSON);
     } catch (error) {
       setError(error);
+    } finally {
+      setLoading(false);
     }
   };
 
-    useEffect(() => {
-    fetchData();
-    }, []); 
+  useEffect(() => {
+      fetchData();
+  }, [username]);
 
-    return {data}
+  return {data, error, loading}
 }
