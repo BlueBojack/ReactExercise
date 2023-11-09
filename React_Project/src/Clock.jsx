0c0f8450b99d160151f1e851fce294d42bb535a6
BlueBojack <curtis.ducasse1g@gmail.com>
2023-11-09 10:23:32 +0100

    added 71

--- React_Project/src/Clock.jsx
+++ React_Project/src/Clock.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./Clock.scss";
+import { LanguageContext } from "./LanguageContext";
 
 export const Clock = () => {
     const [time, setTime] = useState("");
+    const language = useContext(LanguageContext);
   
     function currentTime() {
       const time = new Date().toLocaleTimeString();
@@ -19,7 +21,12 @@ export const Clock = () => {
       };
     }, []);
   
-    return <h2>{time}</h2>;
+    return (
+    <>
+      {language === "it" ? <h2>Orario corrente</h2> : <h2>Current time</h2>}
+      <h1>{time}</h1>
+    </>
+    );
   };
 
 export default Clock;
